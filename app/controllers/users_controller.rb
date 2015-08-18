45d2e6850fb14f6fd3ae777bfbb4c19f18a7e447
william <wdrojas8@gmail.com>
2015-08-18 00:23:13 -0500

    cooregi...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     unless @user == current_user
-      #redirect_to :back, :alert => "Acceso denegado."
+      redirect_to :back, :alert => "Acceso denegado."
     end
   end
 
--- db/migrate/20140224203310_devise_create_users.rb
+++ db/migrate/20140224203310_devise_create_users.rb
@@ -33,7 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
       t.timestamps
     end
-    add_index :users, :name,                 :unique => true
+    # add_index :users, :name,                 :unique => true
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
     add_index :users, :role,                 :unique => true
